aa0975076688786183ddf6483ac04cbf6fa9f61c
mlradhikaimad <mlradhika2016@gmail.com>
2016-09-30 16:28:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 var butt=document.getElementById('counter');
 var counter=0;
 butt.onclick=function(){
-    alert("clicked"+counter);
+   
     counter=counter+1;
     var span=document.getElementById('count');
     span.innerHTML=counter.toString();
