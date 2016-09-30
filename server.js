e1dd1ef3a5291cbd3c824262d04e4aa0f256044e
mlradhikaimad <mlradhika2016@gmail.com>
2016-09-30 12:16:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,21 +4,51 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
-var articleOne={
-  title:'Article1',
-  head:`Article One`,
-  date:'Created on 29th september 2016',
-  content: `<p>
-        This is the first paragraph of first article
-    </p>
-     <p>
-        This is the second paragraph of first article
-    </p>
-     <p>
-        This is the third paragraph of first article
-    </p>`
-  
-      };
+
+      var articles={
+          article-One:
+          {title:'Article1',
+           head:'Article One',
+           date:'Created on 29th september 2016',
+           content: `<p>
+                    This is the first paragraph of first article
+                    </p>
+                    <p>
+                    This is the second paragraph of first article
+                    </p>
+                    <p>
+                    This is the third paragraph of first article
+                    </p>`
+           },
+           article-Two:
+          {title:'Article2',
+           head:'Article Two',
+           date:'Created on 29th september 2016',
+           content: `<p>
+                    This is the first paragraph of second article
+                    </p>
+                    <p>
+                    This is the second paragraph of second article
+                    </p>
+                    <p>
+                    This is the third paragraph of second article
+                    </p>`
+           },
+           article-Three:
+          {title:'Article3',
+           head:'Article Three',
+           date:'Created on 29th september 2016',
+           content: `<p>
+                    This is the first paragraph of third article
+                    </p>
+                    <p>
+                    This is the second paragraph of third article
+                    </p>
+                    <p>
+                    This is the third paragraph of third article
+                    </p>`
+           }
+                };
 function createTemplate(data)
 {
     var title=data.title;
@@ -67,19 +97,13 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one',function(req,res)
+app.get('/:articleName',function(req,res)
 {
      //res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
-    res.send( createTemplate(articleOne));
+   var articleName=req.paras.articleName;
+    res.send( createTemplate(articleName));
 });
-app.get('/article-two',function(req,res)
-{
-     res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
-app.get('/article-three',function(req,res)
-{
-    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
-});
+
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
