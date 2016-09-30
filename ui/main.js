f8bf22331354c272f385ebb668857b2da8e37453
mlradhikaimad <mlradhika2016@gmail.com>
2016-09-30 17:17:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -14,5 +14,19 @@ butt.onclick=function(){
    };
    request.open('GET','http://mlradhikaimad.imad.hasura-app.io/counter',true);
    request.send(null);
-    
 };
+   var nameInput=document.getElementById('name');
+   var nam=nameInput.value;
+  var submit=document.getElementById('submit_btn') ;
+  submit.onclick=function()
+  {
+      var names=['name1','name2','name3','name4'];
+      var list='';
+      for(var i=0;i<names.length;i++)
+      list+='<l1>'+names[i]+'</l1>';
+      
+  
+  var ul=document.getElementById('namelist');
+  u1.innerHTNL=list;
+  };
+
