909196e0958a8e833ed817ea2ea9bf615269f7ca
mlradhikaimad <mlradhika2016@gmail.com>
2016-09-30 16:38:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,11 +3,13 @@ var counter=0;
 butt.onclick=function(){
    var request=new XMLHttpRequest();    
    request.onreadystatechange=function(){
-       if(request.status===200)
+       if(request.readystate===XMLHttpRequest.DONE)
+       {
+           if(request.status===200)
        {
            var counter=request.responseText
     var span=document.getElementById('count');
-    span.innerHTML=counter.toString();       
+    span.innerHTML=counter.toString();      } 
        }
    };
    
