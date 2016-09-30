4434d3bad3d5c602c3e1ea441c5bb9efd1483aaa
mlradhikaimad <mlradhika2016@gmail.com>
2016-09-30 14:19:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 console.log('Loaded!');
 var element=document.getElementById('main-text');
 element.innerHTML="New Value";
-var ele=document.getElementById('madi');
+var ele=document.getElementById('img1');
 ele.onclick=function()
 {
     ele.style.marginLeft='50x';
