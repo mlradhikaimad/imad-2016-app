07dded1afeab56a308114b42780b576ee9bbfcb7
mlradhikaimad <mlradhika2016@gmail.com>
2016-09-30 11:47:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -70,7 +70,7 @@ app.get('/', function (req, res) {
 app.get('/article-one',function(req,res)
 {
      //res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
-    res.send( createTemplate(articleone));
+    res.send( createTemplate(articleOne));
 });
 app.get('/article-two',function(req,res)
 {
