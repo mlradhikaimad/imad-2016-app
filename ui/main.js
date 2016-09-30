cd8300324d23596b806e61644c83cdc6f2f47c5e
mlradhikaimad <mlradhika2016@gmail.com>
2016-09-30 14:54:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,12 +7,14 @@ function moveRight()
 {
   marginLeft=marginLeft+50;
   ele.style.marginLeft=marginLeft+'px';
+  alert(marginLeft);
 }
 ele.onclick=function()
 {
    
-    alert("moving image");
+    alert(margin);
+    
    var interval=setInterval(moveRight,50);
-   alert(margiLeft);
+
    
 };
