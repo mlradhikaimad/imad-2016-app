0e910efff06b5b848f698e1d3839da7c926b9bec
mlradhikaimad <mlradhika2016@gmail.com>
2016-09-30 17:19:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -16,7 +16,7 @@ butt.onclick=function(){
    request.send(null);
 };
    var nameInput=document.getElementById('name');
-   var name=nameInput.value;
+   var nam=nameInput.value;
   var submit=document.getElementById('submit_btn') ;
   submit.onclick=function()
   {
